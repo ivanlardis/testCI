a35092414f5b289cfcff71492e5ebdb276e1e990
ivanlardis <ivan24031993@yandex.ru>
2017-02-12 20:29:01 +0300

    -тест

--- app/src/main/java/com/example/black_sony/testci/MainActivity.java
+++ app/src/main/java/com/example/black_sony/testci/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.black_sony.testci;
 
+import org.w3c.dom.Text;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView= (TextView) findViewById(R.id.textTTT);
+    textView.setText(""+getResources().getString(R.string.mainView));
+
     }
 }
